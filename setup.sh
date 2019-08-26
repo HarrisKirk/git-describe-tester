39c1f59d76b0d8b2c083e14ceb3f9b5db9af008d
Harris Kirk <harris.kirk@lumiradx.com>
2019-08-26 09:26:10 -0400

    Add graph

--- setup.sh
+++ setup.sh
@@ -3,6 +3,10 @@ addCommit() {
     git add README.md
     git commit --quiet -m "$1"
 }
+graph() {
+    git log --graph --full-history --all --color --pretty=format:"%x1b[31m%h%x09%x1b[32m%d%x1b[0m%x20%s" --simplify-by-decoration
+}
+
 repo_root="./build"
 repo_name="mygitrepo"
 
@@ -19,6 +23,9 @@ git tag -m "Annotated" 0.0.1
 
 addCommit "next commit"
 addCommit "next commit"
+
+graph 
+
 git describe --always
 
 
