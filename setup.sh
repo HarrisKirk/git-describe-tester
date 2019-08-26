5c6d34f6196608a8dfeb45429b442f3afdaa1ef7
Harris Kirk <harris.kirk@lumiradx.com>
2019-08-26 09:10:34 -0400

    Add better script ;)

--- setup.sh
+++ setup.sh
@@ -1,11 +1,25 @@
+addCommit() {
+    echo `date` >> README.md
+    git add README.md
+    git commit -m "$1"
+}
+
 repo_root="./build"
 repo_name="mygitrepo"
 
-rm -rf repo_root
-
-
+rm -rf ${repo_root}
 mkdir -p ${repo_root}/${repo_name}
-cd $repo_root/${repo_name}
+cd ${repo_root}/${repo_name}
+
 git init
+git status
+
+addCommit "First commit"
+git tag -m "Annotated" 0.0.1 
+
+git describe --always
+
+
+
 
 echo "Done"
